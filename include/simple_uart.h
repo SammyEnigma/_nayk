f4460db92c360734e6f96e5b6dc1d3396f942e41
nayk1982 <sutcedortal@gmail.com>
2020-02-02 18:46:48 +0300

    Communications port changed. Not Finished

--- include/simple_uart.h
+++ include/simple_uart.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+** Copyright (c) 2020 Evgeny Teterin (nayk) <sutcedortal@gmail.com>
+** All right reserved.
+**
+** Permission is hereby granted, free of charge, to any person obtaining
+** a copy of this software and associated documentation files (the
+** "Software"), to deal in the Software without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Software, and to
+** permit persons to whom the Software is furnished to do so, subject to
+** the following conditions:
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
 #ifndef SIMPLE_UART_H
 #define SIMPLE_UART_H
 
@@ -41,14 +65,13 @@ public:
     qint64 read(char *bytes, qint64 count) override;
     QString portSettings();
     static QStringList availablePorts();
-    static QStringList availableBaudRates();
 #if defined (QT_GUI_LIB)
     static void fillComboBoxPortName(QComboBox *comboBox, const QVariant &defaultValue = QVariant());
 #endif
 
 private:
     QString m_portName;
-    qint32 m_baudRate;
+    qint32 m_baudRate {9600};
 
 #if defined (Q_OS_WIN32)
     HANDLE m_handle {INVALID_HANDLE_VALUE};
@@ -56,6 +79,7 @@ private:
     int m_handle {-1};
 #endif
 
+    int baudRateConst(qint32 baudRate);
 };
 //==============================================================================
 
--- sources/simple_uart.cpp
+++ sources/simple_uart.cpp
@@ -1,3 +1,27 @@
+/****************************************************************************
+** Copyright (c) 2020 Evgeny Teterin (nayk) <sutcedortal@gmail.com>
+** All right reserved.
+**
+** Permission is hereby granted, free of charge, to any person obtaining
+** a copy of this software and associated documentation files (the
+** "Software"), to deal in the Software without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Software, and to
+** permit persons to whom the Software is furnished to do so, subject to
+** the following conditions:
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
 #include <QDir>
 #include <stdio.h>
 #include "Convert"
@@ -12,23 +36,13 @@
 
 namespace nayk { //=============================================================
 
-//==============================================================================
-
-#if defined (Q_OS_WIN32)
-const int baudRates[10] = {CBR_300, CBR_600, CBR_1200, CBR_2400, CBR_4800, CBR_9600, CBR_19200, CBR_38400, CBR_57600, CBR_115200};
-#endif
-
-#if defined (Q_OS_LINUX)
-const int baudRates[10] = {B300, B600, B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200};
-#endif
-
-const int baudRatesVal[10] = {300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
 const int portWaitTimeout {5000};
 
 //==============================================================================
 SimpleUart::SimpleUart(QObject *parent) : AbstractPort(parent)
 {
-
+    QStringList ports = availablePorts();
+    if(!ports.isEmpty()) m_portName = ports.first();
 }
 //==============================================================================
 SimpleUart::SimpleUart(const QString &portName, QObject *parent)
@@ -49,7 +63,76 @@ bool SimpleUart::setPortName(const QString &portName)
 //==============================================================================
 bool SimpleUart::setBaudRate(qint32 baudRate)
 {
+#ifdef Q_OS_LINUX
+    struct termios options;
 
+    if( tcgetattr(m_handle, &options) < 0) {
+
+        m_lastError = tr("%1: Failed to get termios")
+                        .arg(m_portName);
+#if !defined (WITHOUT_LOG)
+        emit toLog( m_lastError, Log::LogError);
+#endif
+        return false;
+    }
+
+    if(cfsetispeed(&options, baudRateConst(baudRate)) < 0) {
+
+        m_lastError = tr("%1: Failed to set baudrate")
+                        .arg(m_portName);
+#if !defined (WITHOUT_LOG)
+        emit toLog( m_lastError, Log::LogError);
+#endif
+        return false;
+    }
+
+    if(cfsetospeed(&options, baudRateConst(baudRate)) < 0) {
+
+        m_lastError = tr("%1: Failed to set baudrate")
+                        .arg(m_portName);
+#if !defined (WITHOUT_LOG)
+        emit toLog( m_lastError, Log::LogError);
+#endif
+        return false;
+    }
+
+    if (tcsetattr(m_handle, TCSANOW, &options) < 0) {
+
+        m_lastError = tr("%1: Failed to set termios")
+                        .arg(m_portName);
+#if !defined (WITHOUT_LOG)
+        emit toLog( m_lastError, Log::LogError);
+#endif
+        return false;
+    }
+#endif // LINUX
+
+#ifdef WINDOWS
+    DCB dcb;
+    BOOL fSuccess;
+
+    fSuccess = GetCommState(m_handle, &dcb);
+    if (!fSuccess) {
+        if (logComFile) {
+            logComFile->write("ERROR - in GetCommState()\n");
+            logComFile->flush();
+        }
+        return false;
+    }
+    dcb.BaudRate = baudRateConst(baudRate);	// current baud rate
+
+    fSuccess = SetCommState(m_handle, &dcb);
+    if (!fSuccess) {
+        if (logComFile) {
+            logComFile->write("ERROR - in SetCommState()\n");
+            logComFile->flush();
+        }
+        return false;
+    }
+#endif
+
+    m_baudRate = baudRate;
+    return true;
 }
 //==============================================================================
 QString SimpleUart::portName() const
@@ -129,7 +212,7 @@ bool SimpleUart::open(bool readOnly)
     }
 
     // Baudrate
-    if(cfsetispeed(&options, baudRates[m_baudRate]) < 0) {
+    if(cfsetispeed(&options, baudRateConst(m_baudRate)) < 0) {
 
         m_lastError = tr("%1: Failed to set baudrate")
                         .arg(m_portName);
@@ -141,7 +224,7 @@ bool SimpleUart::open(bool readOnly)
         return false;
     }
 
-    if(cfsetospeed(&options, baudRates[m_baudRate]) < 0) {
+    if(cfsetospeed(&options, baudRateConst(m_baudRate)) < 0) {
 
         m_lastError = tr("%1: Failed to set baudrate")
                         .arg(m_portName);
@@ -229,7 +312,7 @@ bool SimpleUart::open(bool readOnly)
         return false;
     }
 
-    dcb.BaudRate = baudRates[m_baudRate];	// current baud rate
+    dcb.BaudRate = baudRateConst(m_baudRate);	// current baud rate
     dcb.fBinary = 1;			// binary mode, no EOF check
     dcb.fParity = 0;			// disable parity checking
     dcb.fOutxCtsFlow = 0;		// CTS output flow control
@@ -261,7 +344,7 @@ bool SimpleUart::open(bool readOnly)
         return false;
     }
 
-    fSuccess = SetupComm( m_handle, 1048576, 1048576);
+    fSuccess = SetupComm( m_handle, 4096, 4096);
 
     if (!fSuccess) {
         if (logComFile) {
@@ -300,12 +383,29 @@ bool SimpleUart::open(bool readOnly)
                 , Log::LogDbg );
 #endif
 
+    emit portOpen();
     return true;
 }
 //==============================================================================
 void SimpleUart::close()
 {
+    if(!isOpen()) return;
 
+#ifdef Q_OS_LINUX
+    lockf(m_handle, F_ULOCK, 0);
+    ::close(m_handle);
+    m_handle = -1;
+#endif
+#ifdef Q_OS_WIN32
+    PurgeComm( m_handle, PURGE_TXCLEAR | PURGE_RXCLEAR );
+    CloseHandle(m_handle);
+    m_handle = INVALID_HANDLE_VALUE;
+#endif
+
+#if !defined (WITHOUT_LOG)
+    emit toLog( tr("%1: Port is closed").arg(m_portName), Log::LogInfo );
+#endif
+    emit portClose();
 }
 //==============================================================================
 bool SimpleUart::isOpen() const
@@ -520,16 +620,6 @@ QStringList SimpleUart::availablePorts()
 #endif
 }
 //==============================================================================
-QStringList SimpleUart::availableBaudRates()
-{
-    QStringList baudratesList;
-
-    for(int i=0; i<10; ++i)
-        baudratesList << QString::number(baudRatesVal[i]);
-
-    return baudratesList;
-}
-//==============================================================================
 #if defined (QT_GUI_LIB)
 void SimpleUart::fillComboBoxPortName(QComboBox *comboBox, const QVariant &defaultValue)
 {
@@ -550,6 +640,78 @@ void SimpleUart::fillComboBoxPortName(QComboBox *comboBox, const QVariant &defau
 }
 #endif
 //==============================================================================
+int SimpleUart::baudRateConst(qint32 baudRate)
+{
+    switch (baudRate) {
+    case 300:
+#ifdef Q_OS_WIN32
+        return CBR_300;
+#else
+        return B300;
+#endif
+    case 600:
+#ifdef Q_OS_WIN32
+        return CBR_600;
+#else
+        return B600;
+#endif
+    case 1200:
+#ifdef Q_OS_WIN32
+        return CBR_1200;
+#else
+        return B1200;
+#endif
+    case 2400:
+#ifdef Q_OS_WIN32
+        return CBR_2400;
+#else
+        return B2400;
+#endif
+    case 4800:
+#ifdef Q_OS_WIN32
+        return CBR_4800;
+#else
+        return B4800;
+#endif
+    case 9600:
+#ifdef Q_OS_WIN32
+        return CBR_9600;
+#else
+        return B9600;
+#endif
+    case 19200:
+#ifdef Q_OS_WIN32
+        return CBR_19200;
+#else
+        return B19200;
+#endif
+    case 38400:
+#ifdef Q_OS_WIN32
+        return CBR_38400;
+#else
+        return B38400;
+#endif
+    case 57600:
+#ifdef Q_OS_WIN32
+        return CBR_57600;
+#else
+        return B57600;
+#endif
+    case 115200:
+#ifdef Q_OS_WIN32
+        return CBR_115200;
+#else
+        return B115200;
+#endif
+    default:
+#ifdef Q_OS_WIN32
+        return CBR_0;
+#else
+        return B0;
+#endif
+    }
+}
+//==============================================================================
 
 
 
@@ -559,352 +721,6 @@ void SimpleUart::fillComboBoxPortName(QComboBox *comboBox, const QVariant &defau
 
 /*
 
-int ComAPI::openCom(const QString &portName, int speedIndex)
-{
-    closeCom();
-
-#ifdef LINUX
-    m_portName = "/dev/" + portName;
-    int fd = open(m_portName.toLatin1().constData(), O_RDWR | O_NOCTTY | O_NDELAY);
-
-    if (fd < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - open\n");
-            logComFile->flush();
-        }
-
-        return -1;
-    }
-
-    if( lockf(fd, F_TEST, 0) < 0 ) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - lockf(fd, F_TEST, 0)\n");
-            logComFile->flush();
-        }
-
-        close(fd);
-        return -1;
-    }
-    else if( lockf(fd, F_LOCK, 0) < 0 ) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - lockf(fd, F_LOCK, 0)\n");
-            logComFile->flush();
-        }
-
-        close(fd);
-        return -1;
-    }
-
-    if(fcntl(fd, F_SETFL, 0) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - fcntl(fd, F_SETFL, 0)\n");
-            logComFile->flush();
-        }
-
-        lockf(fd, F_ULOCK, 0);
-        close(fd);
-        return -1;
-    }
-
-    struct termios options;
-
-    if (tcgetattr(fd, &options) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - tcgetattr(fd, &options)\n");
-            logComFile->flush();
-        }
-
-        lockf(fd, F_ULOCK, 0);
-        close(fd);
-        return -1;
-    }
-
-    // Baudrate
-    if(cfsetispeed(&options, Baudrates[speedIndex]) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - cfsetispeed(&options, Baudrates[speed])\n");
-            logComFile->flush();
-        }
-
-        lockf(fd, F_ULOCK, 0);
-        close(fd);
-        return -1;
-    }
-
-    if(cfsetospeed(&options, Baudrates[speedIndex]) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - cfsetospeed(&options, Baudrates[speed])\n");
-            logComFile->flush();
-        }
-
-        lockf(fd, F_ULOCK, 0);
-        close(fd);
-        return -1;
-    }
-
-    // Bit size
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS8;
-
-    // Parity
-    options.c_cflag &= ~PARENB;
-    options.c_cflag &= ~PARODD;
-
-    // Stop bits
-    options.c_cflag &= ~CSTOPB;
-
-    // Flow Control
-    options.c_cflag &= ~CRTSCTS;
-    options.c_iflag &= ~(IXON | IXOFF | IXANY);
-
-    options.c_cflag |= (CLOCAL | CREAD);
-
-
-    options.c_iflag &= ~INLCR;
-    options.c_iflag &= ~ICRNL;
-    options.c_iflag &= ~IGNCR;
-    options.c_oflag &= ~OPOST;
-
-    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-    options.c_cc[VMIN] = 0;
-    options.c_cc[VTIME] = 1;
-
-    if (tcsetattr(fd, TCSANOW, &options) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - tcsetattr(fd, TCSANOW, &options)\n");
-            logComFile->flush();
-        }
-
-        lockf(fd, F_ULOCK, 0);
-        close(fd);
-        return -1;
-    }
-
-    comId = fd;
-#endif //LINUX
-
-#ifdef WINDOWS
-    DCB dcb;
-    BOOL fSuccess;
-    m_portName = "\\\\.\\" + portName;
-
-    comId = CreateFileA(reinterpret_cast<LPCSTR>(m_portName.toLatin1().data()),
-                        GENERIC_READ | GENERIC_WRITE,
-                        0,			// comm devices must be opened w/exclusive-access
-                        NULL,			// no security attrs
-                        OPEN_EXISTING,          // comm devices must use OPEN_EXISTING
-                        0,			// FILE_FLAG_OVERLAPPED, not overlapped I/O
-                        NULL			// hTemplate must be NULL for comm devices
-                        );
-
-
-    if ( comId == INVALID_HANDLE_VALUE) {
-        if (logComFile) {
-            logComFile->write("ERROR - INVALID_HANDLE_VALUE\n");
-            logComFile->flush();
-        }
-        return -1;
-    }
-
-    fSuccess = GetCommState( comId, &dcb);
-
-    if (!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in GetCommState()\n");
-            logComFile->flush();
-        }
-        return -1;
-    }
-
-    dcb.BaudRate = Baudrates[speedIndex];	// current baud rate
-    dcb.fBinary = 1;			// binary mode, no EOF check
-    dcb.fParity = 0;			// disable parity checking
-    dcb.fOutxCtsFlow = 0;		// CTS output flow control
-    dcb.fOutxDsrFlow = 0;		// DSR output flow control
-    dcb.fDtrControl = DTR_CONTROL_DISABLE;
-    // DTR flow control type
-    dcb.fDsrSensitivity = 0;	// DSR sensitivity
-    dcb.fTXContinueOnXoff = 0;	// XOFF continues Tx
-    dcb.fOutX = 0;		// XON/XOFF out flow control
-    dcb.fInX = 0;		// XON/XOFF in flow control
-    //	dcb.XoffLim=128;
-    //	dcb.XonLim=256;
-    dcb.fErrorChar = 0;		// enable error replacement
-    dcb.fNull = 0;		// enable null stripping
-    dcb.fRtsControl = RTS_CONTROL_HANDSHAKE;
-    // RTS flow control
-    dcb.fAbortOnError = 1;	// abort reads/writes on error
-    dcb.ByteSize = 8;           // number of bits/byte, 4-8
-    dcb.Parity = 0;             // 0-4=no,odd,even,mark,space
-    dcb.StopBits = 0;           // 0,1,2 = 1, 1.5, 2
-
-    fSuccess = SetCommState( comId, &dcb);
-
-    if (!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in SetCommState()\n");
-            logComFile->flush();
-        }
-        return -2;
-    }
-
-    fSuccess = SetupComm( comId, 1048576, 1048576);
-
-    if (!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in SetupComm()\n");
-            logComFile->flush();
-        }
-        return -3;
-    }
-
-    COMMTIMEOUTS tOut;
-    tOut.ReadIntervalTimeout = 3;
-    tOut.ReadTotalTimeoutMultiplier = 2;
-    tOut.ReadTotalTimeoutConstant = 50;
-    tOut.WriteTotalTimeoutMultiplier = 2;
-    tOut.WriteTotalTimeoutConstant = 50;
-
-    fSuccess = SetCommTimeouts(comId, &tOut );
-
-    if(!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in SetCommTimeouts()\n");
-            logComFile->flush();
-        }
-        return -4;
-    }
-
-    PurgeComm( comId, PURGE_TXCLEAR | PURGE_RXCLEAR );
-
-#endif //WINDOWS
-
-    if (logComFile) {
-
-        QString log = QString("%1\tPort is open: %2\n")
-                .arg(QDateTime::currentDateTime().toString("hh:mm:ss:zzz"))
-                .arg(portSettings());
-        logComFile->write(log.toLatin1());
-        logComFile->flush();
-    }
-
-    return 0;
-}
-//==============================================================================
-int ComAPI::closeCom()
-{
-    if(!isOpen()) return 0;
-
-#ifdef LINUX
-    lockf(comId, F_ULOCK, 0);
-    close(comId);
-    comId = -1;
-#endif
-#ifdef WINDOWS
-    PurgeComm( comId, PURGE_TXCLEAR | PURGE_RXCLEAR );
-    CloseHandle(comId);
-    comId = INVALID_HANDLE_VALUE;
-#endif
-
-    if (logComFile) {
-        QString log = QString("%1\tPort is close\n")
-                .arg(QDateTime::currentDateTime().toString("hh:mm:ss:zzz"));
-        logComFile->write(log.toLatin1());
-        logComFile->flush();
-    }
-
-    return 0;
-}
-//==============================================================================
-int ComAPI::changeSpeedCom(int speedIndex)
-{
-#ifdef LINUX
-    struct termios options;
-
-    if( tcgetattr(comId, &options) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - tcgetattr(comId, &options)\n");
-            logComFile->flush();
-        }
-
-        return -1;
-    }
-
-    if(cfsetispeed(&options, Baudrates[speedIndex]) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - cfsetispeed(&options, Baudrates[speed])\n");
-            logComFile->flush();
-        }
-
-        return -1;
-    }
-
-    if(cfsetospeed(&options, Baudrates[speedIndex]) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - cfsetospeed(&options, Baudrates[speed])\n");
-            logComFile->flush();
-        }
-
-        return -1;
-    }
-
-    if (tcsetattr(comId, TCSANOW, &options) < 0) {
-
-        if (logComFile) {
-            logComFile->write("ERROR - set baudrate\n");
-            logComFile->flush();
-        }
-
-        return -1;
-    }
-#endif
-
-#ifdef WINDOWS
-    DCB dcb;
-    BOOL fSuccess;
-
-    fSuccess = GetCommState(comId, &dcb);
-    if (!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in GetCommState()\n");
-            logComFile->flush();
-        }
-        return -1;
-    }
-    dcb.BaudRate = Baudrates[speedIndex];	// current baud rate
-
-    fSuccess = SetCommState(comId, &dcb);
-    if (!fSuccess) {
-        if (logComFile) {
-            logComFile->write("ERROR - in SetCommState()\n");
-            logComFile->flush();
-        }
-        return -3;
-    }
-#endif
-
-    if (logComFile) {
-        QString log = QString("%1\tPort change speed: %2\n")
-                .arg(QDateTime::currentDateTime().toString("hh:mm:ss:zzz"))
-                .arg(portSettings());
-        logComFile->write(log.toLatin1());
-        logComFile->flush();
-    }
-    return 0;
-}
-//==============================================================================
 QString ComAPI::portSettings()
 {
     if(!isOpen()) return QString();
